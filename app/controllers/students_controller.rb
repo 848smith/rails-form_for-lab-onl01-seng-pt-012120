75e9f6db0a707194edc919e9fb66e2fdece55cc5
Andrew Smith <848smith@gmail.com>
2020-06-22 07:21:34 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -19,6 +19,8 @@ class StudentsController < ApplicationController
   
   def update
     @student = Student.find(params[:id])
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
   end
   
   private
